189d951b2a24d26bc7eb1b219b2d98e45d758dc5
小王匠 <wsncoder@163.com>
2024-04-16 16:28:00 +0800

    feat: 增加直线光带掠过城市效果

--- src/three/modify/modifyCityMaterial.js
+++ src/three/modify/modifyCityMaterial.js
@@ -12,6 +12,7 @@ export default function modifyCityMaterial(mesh) {
 
     addGradColor(shader, mesh);
     addSpread(shader);
+    addLightLine(shader);
   };
 }
 
@@ -111,6 +112,42 @@ export function addSpread(shader) {
   gsap.to(shader.uniforms.uSpreadTime, {
     value: 800,
     duration: 3,
+    ease: "none",
+    repeat: -1,
+  });
+}
+
+
+export function addLightLine(shader) {
+  //   扩散的时间
+  shader.uniforms.uLightLineTime = { value: -1500 };
+  //   设置条带的宽度
+  shader.uniforms.uLightLineWidth = { value: 200 };
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "#include <common>",
+    `
+        #include <common>
+        uniform float uLightLineTime;
+        uniform float uLightLineWidth;
+        `
+  );
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    "//#end#",
+    `
+      float LightLineMix = -(vPosition.x+vPosition.z-uLightLineTime)*(vPosition.x+vPosition.z-uLightLineTime)+uLightLineWidth;
+
+      if(LightLineMix>0.0){
+          gl_FragColor = mix(gl_FragColor,vec4(0.8,1.0,1.0,1),LightLineMix /uLightLineWidth);
+      }
+      //#end#
+      `
+  );
+
+  gsap.to(shader.uniforms.uLightLineTime, {
+    value: 1500,
+    duration: 5,
     ease: "none",
     repeat: -1,
   });
