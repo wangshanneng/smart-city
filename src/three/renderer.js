f50b9c5e03e0a0cd5102b0cfbb7bed7d90747752
小王匠 <wsncoder@163.com>
2024-04-21 21:24:45 +0800

    fix: 设置抗锯齿效果

--- src/three/renderer.js
+++ src/three/renderer.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 
 // 初始化渲染器
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, // 开启抗锯齿
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true; // 启用阴影映射
 
