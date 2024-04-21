20bdd9dbfb845413d37b5118a122da015b58ef2d
小王匠 <wsncoder@163.com>
2024-04-22 01:35:57 +0800

    feat: 搭建大屏样式与结构

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import Scene from '@/components/Scene.vue'
+import BigScreen from '@/components/BigScreen.vue'
 </script>
 
 <template>
   <main>
     <h1>测试</h1>
     <Scene />
+    <BigScreen />
   </main>
 </template>
